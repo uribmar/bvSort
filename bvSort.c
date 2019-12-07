5a7706c6ab3400fdd018a808e367c321dd210b97
uribmar <uribmar@bvu.edu>
2019-12-06 19:19:46 -0600

    added support code to have multiple threads

--- bvSort.c
+++ bvSort.c
@@ -1,10 +1,31 @@
 #include "helpers.h"
 
 //number of files that we have created
+long inputFileSize;
 int fileCount = 0;
 
+
+//calculate number of temp files that need to be created based on input file size
+void countFiles(char* filename) {
+  int f = open(filename, O_RDONLY);
+  inputFileSize = lseek(f, 0L, SEEK_END);
+
+  printf("Size of input file: %ld bytes\n", inputFileSize);
+
+  //int divide it into chunks of 250MB
+  fileCount = inputFileSize / MAX_DATA_SIZE;
+  if( (inputFileSize%MAX_DATA_SIZE) != 0) {
+    fileCount++;
+  }
+}
+
+
 //makes all of the temp files that are needed for the sort
-void makeFiles(char* filename) {
+void* makeFiles(void* arg) {
+  threadInfo info = *(threadInfo*)arg;
+  printf("I'm thread number %d and the file is named %s\n",
+         info.tID, info.filename);
+  /*
   //file decriptor for the file we will read in the data to be sorted from
   int rf = open(filename, O_RDONLY);
 
@@ -38,6 +59,7 @@ void makeFiles(char* filename) {
 
   free(data);
   close(rf);
+  */
 }
 
 int main(int argc, char** argv) {
@@ -46,7 +68,21 @@ int main(int argc, char** argv) {
     return -1;
   }
 
-  makeFiles(argv[1]);
+  countFiles(argv[1]);
+
+  //initialize threads to make files
+  pthread_t tID[NUM_CORES];
+  threadInfo info[NUM_CORES];
+  for(int i=0; i<NUM_CORES; i++) {
+    info[i].filename = argv[1];
+    info[i].tID = i;
+    pthread_create(tID+i, 0, makeFiles, info+i);
+  }
+
+  //join threads
+  for(int i=0; i<NUM_CORES; i++) {
+    pthread_join(tID[i], NULL);
+  }
 
   //TODO open input file
   //TODO sort the sumbitch
--- helpers.h
+++ helpers.h
@@ -6,8 +6,18 @@
 #include <errno.h>
 #include <string.h>
 #include <fcntl.h>
+#include <pthread.h>
 
+//256MB file
 int MAX_DATA_SIZE = 268435456;
+int NUM_CORES = 2;
+
+
+struct thready {
+  char* filename;
+  int tID;
+} typedef threadInfo;
+
 
 void printFile(char* filename) {
   int fd = open(filename, O_RDONLY),
